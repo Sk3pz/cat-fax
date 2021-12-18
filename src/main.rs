b347371e6e3c9398daf3bed5575a9793a8542070
eric <skepz.dev@gmail.com>
2021-12-18 01:56:24 -0600

    prototype

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 mod lol;
+mod window;
 
 use std::os;
 use better_term::rainbowify;
 use rand::Rng;
+use crate::window::cat_fax_window;
 use crate::lol::{Control, print_chars_lol};
+
+const FONT_SIZE: u32 = 16;
+const WINDOW_HEIGHT: u32 = 400;
 
 #[derive(Debug, Clone)]
 struct TerminalArgs {
@@ -11,6 +16,7 @@ struct TerminalArgs {
     raw: bool,
 }
 
+/// parse the command line arguments into a struct
 fn parse_args(args: Vec<String>) -> Option<TerminalArgs> {
     if !args.contains(&"-t".to_string()) {
         return None
@@ -20,55 +26,76 @@ fn parse_args(args: Vec<String>) -> Option<TerminalArgs> {
         raw: false,
     };
     for x in 0..args.len() {
-        let a = args.get(x).unwrap();
-        match a.as_str() {
+        let s = args.get(x).unwrap();
+        match s.as_str() {
             "-c" | "--color" => {
+                if x + 1 < args.len() {
+                    let next = args.get(x + 1).unwrap();
+                    if next == "false" {
+                        targs.color = false;
+                        continue;
+                    }
+                }
+                if targs.raw {
+                    println!("Failed to set color mode: raw mode can not be colored!");
+                    continue;
+                }
                 targs.color = true;
             }
             "-r" | "--raw" => {
+                if x + 1 < args.len() {
+                    let next = args.get(x + 1).unwrap();
+                    if next == "false" {
+                        targs.raw = false;
+                        continue;
+                    }
+                }
                 targs.raw = true;
+                targs.color = false; // raw mode is always colorless
             }
             "-t" => {}
-            _ => {
-                println!("unrecognized argument: {}", a);
-                std::process::exit(1);
-            }
+            _ => {}
         }
     }
     Some(targs)
 }
 
 /// prints the cat fact formatted to the terminal
-fn cat_fax_term(targs: TerminalArgs, fact: &str) {
-
-
-
+fn cat_fax_term(targs: TerminalArgs, fact: &str, num: usize) {
+    let fax_num = format!("Cat Fax #{}", num);
+    // if its raw, then just print the fact
     let ascii = if targs.raw {
-        format!("{}", fact)
-    } else {
-        format!("+{line}+\n\
+        format!("{}: {}", fax_num, fact)
+    } else { // otherwise, format it
+        format!("+{tline}{}{tline}{}+\n\
     |  /\\_/\\{spaces}|\n\
     | ( o.o ) {} |\n\
     |  > ^ <{spaces}|\n\
-    +{line}+\n\
-    ", fact,
-                spaces = " ".repeat(fact.len() + 3),
-                line = "-".repeat(fact.len() + 10))
-    };
+    +{}+\n\
+    ",
+                fax_num,
+                if fax_num.len() % 2 != 0 { "-" }
+                else { "" },
+                fact, "-".repeat(fact.len() + 10),
+                tline = "-".repeat((((fact.len() + 10) - fax_num.len()) / 2)),
+                spaces = " ".repeat(fact.len() + 3))};
 
-    let mut lol_ctrl = Control {
-        seed: 0.0,
-        spread: 3.0,
-        frequency: 0.1,
-        background_mode: false,
-        dialup_mode: false,
-        print_color: targs.color
-    };
-    print_chars_lol(ascii.chars(), &mut lol_ctrl, false);
-}
-
-fn cat_fax_window(fact: &str) {
-    println!("This is not yet implemented!");
+    if targs.color {
+        // settings for the lolcat code
+        let mut lol_ctrl = Control {
+            seed: 0.0,
+            spread: 3.0,
+            frequency: 0.1,
+            background_mode: false,
+            dialup_mode: false,
+            print_color: targs.color
+        };
+        // print the fact in color
+        print_chars_lol(ascii.chars(), &mut lol_ctrl, false);
+    } else {
+        // print the fact in plane ascii
+        println!("{}", ascii);
+    }
 }
 
 fn main() {
@@ -77,16 +104,17 @@ fn main() {
     args.remove(0); // remove the program name
     let targs = parse_args(args);
     // get the cat fax from the file
-    let fax = include_str!("fax.txt").split("\n").collect::<Vec<_>>();
+    let fax = include_str!("../assets/fax.txt").split("\n").collect::<Vec<_>>();
 
     // randomly select a cat fax
     let fax_index = rand::thread_rng().gen_range(0, fax.len());
     let fact = *fax.get(fax_index).unwrap();
     drop(fax);
 
+    // display the cat fax to where it needs to go
     if targs.is_some() {
-        cat_fax_term(targs.unwrap(), fact);
+        cat_fax_term(targs.unwrap(), fact, fax_index + 1);
     } else {
-        cat_fax_window(fact);
+        cat_fax_window(fact, fax_index + 1);
     }
 }
