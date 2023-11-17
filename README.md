c0c4599e528c30a2e213b1747ba15534bfb0a15e
eric <skepz.dev@gmail.com>
2023-11-17 16:56:09 -0600

    rewrite

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Install with `cargo install cat-fax` on any OS with rust and cargo installed
 
 On by default, will display the `cat-fax` in the terminal.\
 Flags:\
-`-c` or `--color` - gives colored output (lolcat)\
+`-c` or `--color` - gives colored output\
 `-r` or `--raw`   - prints out all the data on one line with no special formatting\
 Normal output:\
 ![Normal Output Image](screenshots/normal_example.png)\
@@ -17,7 +17,3 @@ Colored output:\
 ![Colored Output Image](screenshots/color_example.png)\
 Raw output:\
 ![Raw Output Image](screenshots/raw_example.png)
-### Window Mode
-Activated with `-w`or `--window`, will display in a separate window\
-Example:\
-![Window Image](screenshots/window_example.png)
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,80 @@
-mod lol;
-mod window;
+use better_term::flush_styles;
+use better_term::fancy::gradient;
+use rand::{Rng, thread_rng};
 
-use rand::Rng;
-use crate::window::cat_fax_window;
-use crate::lol::{Control, print_lines_lol};
+type GradientBound = (u8, u8, u8);
 
-#[derive(Debug, Clone, Copy)]
+const GRADIENTS: [(GradientBound, GradientBound); 5] = [
+    // default gradients
+    ((255, 0, 255), (0, 255, 255)), // magenta -> cyan
+    ((255, 0, 0), (0, 255, 0)), // red -> green
+    ((255, 0, 0), (0, 0, 255)), // red -> blue
+
+    // special gradients
+    ((49, 71, 85), (38, 160, 218)), // cyan
+    ((113, 46, 75), (218, 159, 38)), // orange
+];
+
+const FAX_INCLUDE: &str = include_str!("../assets/fax.txt");
+
+const MAX_LINE_LENGTH: usize = 30;
+
+fn print_gradient<S: Into<String>>(text: S, gradient_start: (u8, u8, u8), gradient_end: (u8, u8, u8)) {
+    let text = text.into();
+    // handle multiline as well
+    let split = text.split('\n').collect::<Vec<&str>>();
+    let length = split.iter().map(|s| s.len()).max().unwrap();
+
+    let gradient = gradient(gradient_start, gradient_end, length);
+
+    for (i, line) in text.lines().enumerate() {
+        for (j, c) in line.chars().enumerate() {
+            let color_index = (i + j) % gradient.len();
+            print!("{}{}", gradient[color_index], c);
+        }
+        println!();
+    }
+}
+
+fn word_wrap(input: &str, max_size: usize) -> Vec<String> {
+    if input.len() <= max_size {
+        return vec![input.to_string()];
+    }
+
+    let mut result = Vec::new();
+    let mut current_line = String::new();
+    let mut current_size = 0;
+
+    for word in input.split_whitespace() {
+        if current_size + word.len() < max_size {
+            // Add word to the current line
+            if !current_line.is_empty() {
+                current_line.push(' ');
+            }
+            current_line.push_str(word);
+            current_size += word.len() + 1;
+        } else {
+            // Start a new line
+            result.push(current_line.clone());
+            current_line = word.to_string();
+            current_size = word.len();
+        }
+    }
+
+    if !current_line.is_empty() {
+        result.push(current_line);
+    }
+
+    result
+}
+
 struct TerminalArgs {
     color: bool,
     raw: bool,
 }
 
-/// parse the command line arguments into a struct
-fn parse_args(args: Vec<String>) -> Option<TerminalArgs> {
-    if args.contains(&"-w".to_string()) || args.contains(&"--window".to_string()) {
-        return None
-    }
-    let mut targs = TerminalArgs {
+fn parse_args(args: Vec<String>) -> TerminalArgs {
+    let mut terminal_args = TerminalArgs {
         color: false,
         raw: false,
     };
@@ -24,131 +82,96 @@ fn parse_args(args: Vec<String>) -> Option<TerminalArgs> {
         let s = args.get(x).unwrap();
         match s.as_str() {
             "-c" | "--color" => {
-                if targs.raw {
+                if terminal_args.raw {
                     println!("Failed to set color mode: raw mode can not be colored!");
                     continue;
                 }
-                targs.color = true;
+                terminal_args.color = true;
             }
             "-r" | "--raw" => {
-                targs.raw = true;
-                targs.color = false; // raw mode is always colorless
+                terminal_args.raw = true;
+                terminal_args.color = false; // raw mode is always colorless
             }
             "-h" | "--help" | "-?" => {
-                println!("+ Cat-Fax Help:\n\
-                |  `-h` or `--help`:   displays this message\n\
-                |  `-w` or `--window`: displays the cat fact in a custom window\n\
-                |  `-c` or `--color`:  displays the cat fact in rainbow\n\
-                |  `-r` or `--raw`:    displays the cat fact raw, no special formatting\n\
-                +");
+                println!("┌ Cat-Fax Help:\n\
+                │  `-h` or `--help`:   displays this message\n\
+                │  `-c` or `--color`:  displays the cat fact in rainbow\n\
+                │  `-r` or `--raw`:    displays the cat fact raw, no special formatting\n\
+                └");
                 std::process::exit(0);
             }
             _ => {}
         }
     }
-    Some(targs)
-}
-
-/// prints the cat fact formatted to the terminal
-fn cat_fax_term(targs: TerminalArgs, fact: &str, num: usize) {
-    let fax_num = format!("Cat Fax #{}", num);
-    // if its raw, then just print the fact
-    let ascii = if targs.raw {
-        format!("{}: {}", fax_num, fact)
-    } else if fact.is_empty() {
-        format!("\
-    ┌{}┐\n\
-    │  /\\_/\\{spaces} │\n\
-    │ ( o.o )   │\n\
-    │  > ^ <{spaces} │\n\
-    └{}┘\n\
-    ", fax_num,
-                "─".repeat(11),
-                spaces = " ".repeat(fact.len() + 3))
-    } else if fax_num.len() > fact.len() + 10 {
-        format!("\
-    ┌{}┐\n\
-    │  /\\_/\\{spaces}│\n\
-    │ ( o.o ) {}{} │\n\
-    │  > ^ <{spaces}│\n\
-    └{}┘\n\
-    ", fax_num, " ".repeat(fax_num.len() - fact.len()), fact,
-                "─".repeat(fact.len() + 10),
-                spaces = " ".repeat(fact.len() + 3))
-    } else { // otherwise, format it
-        let line_size = (fact.len() + 10) - fax_num.len();
-        let left_line_len = "─".repeat(line_size / 2);
-        let right_line_len = "─".repeat(line_size / 2);
-        let mut fact_line = format!("{}{}{}",
-                                    left_line_len,
-                                    fax_num,
-                                    right_line_len);
-        let fl_len = (left_line_len.len() / 3) + (left_line_len.len() / 3) + fax_num.len();
-        if fl_len < (fact.len() + 10) {
-            fact_line += "─";
-        }
-        format!("┌{}┐\n\
-    │  /\\_/\\{spaces}│\n\
-    │ ( o.o ) {} │\n\
-    │  > ^ <{spaces}│\n\
-    └{}┘\n\
-    ", fact_line, fact,
-                "─".repeat(fact.len() + 10),
-                spaces = " ".repeat(fact.len() + 3))};
-
-    if targs.color {
-        // settings for the lolcat code
-        let seed = rand::thread_rng().gen_range(0.0..255.0);
-        let mut lol_ctrl = Control {
-            seed,
-            spread: 3.0,
-            frequency: 0.1,
-            background_mode: false,
-            dialup_mode: false,
-            print_color: true
-        };
-        // print the fact in color
-        print_lines_lol(ascii.split("\n"), &mut lol_ctrl);
-    } else {
-        // print the fact in plane ascii
-        println!("{}", ascii);
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::{cat_fax_term, TerminalArgs};
-
-    #[test]
-    fn term_sweep() {
-        let mut targs = TerminalArgs {
-            color: false,
-            raw: false,
-        };
-        let fax = include_str!("../assets/fax.txt").split("\n").collect::<Vec<_>>();
-        for x in 0..fax.len() {
-            cat_fax_term(targs, fax.get(x).unwrap(), x)
-        }
-    }
+    terminal_args
 }
 
 fn main() {
-    // get the command line arguments
-    let mut args = std::env::args().collect::<Vec<_>>();
-    args.remove(0); // remove the program name
-    let targs = parse_args(args);
-    // get the cat fax from the file
-    let fax = include_str!("../assets/fax.txt").split("\n").collect::<Vec<_>>();
+    println!();
+    println!();
+    let arguments = std::env::args().collect::<Vec<String>>();
+    let terminal_args = parse_args(arguments);
 
-    // randomly select a cat fax
-    let fax_index = rand::thread_rng().gen_range(0..fax.len());
-    let fact = *fax.get(fax_index).unwrap();
-    drop(fax);
+    let facts = FAX_INCLUDE.split('\n').map(|s| s.to_string()).collect::<Vec<String>>();
 
-    // display the cat fax to where it needs to go
-    if targs.is_some() {
-        cat_fax_term(targs.unwrap(), fact, fax_index + 1);
-    } else {
-        cat_fax_window(fact, fax_index + 1);
+    // get a random fact from the list of facts
+    let fact_num = thread_rng().gen_range(0..facts.len());
+    let fact = facts[fact_num].clone();
+
+    if terminal_args.raw {
+        println!("Cat Fax #{}: {}", fact_num + 1, fact);
+        println!();
+        println!();
+        return;
     }
+
+    // handle if the fact is too long
+    let fact_vec = word_wrap(fact.as_str(), MAX_LINE_LENGTH);
+
+    let fact_len = fact_vec.iter().map(|s| s.len()).max().unwrap();
+
+    // generate the box
+    let spaces = " ".repeat(2 + fact_len);
+
+    let num_display = fact_num + 1;
+
+    let top_line = format!("   ┌─ Cat Fact {}┐\n /\\_/\\ {spaces}│", "─".repeat(fact_len - 6));
+    let btm_line = format!("\n   └{} #{} ─┘", "─".repeat(fact_len - 1),
+                           if num_display < 10 {
+                               format!("0{}", num_display)
+                           } else {
+                               format!("{}", num_display)
+                           });
+
+
+    let first = fact_vec[0].clone();
+
+    let mut cat_box = format!("{top_line}\
+                                \n( o.o ) {}{} │", first, " ".repeat(fact_len - first.len()));
+
+    if fact_vec.len() > 1 {
+        for (x, fact) in fact_vec.iter().enumerate().skip(1) {
+            if x == 1  {
+                cat_box.push_str(&format!("\n > ^ <  {}{} │", fact, " ".repeat(fact_len - fact.len())));
+            } else {
+                cat_box.push_str(&format!("\n   │    {}{} │", fact, " ".repeat(fact_len - fact.len())));
+            }
+        }
+        cat_box.push_str(&format!("\n   │   {spaces}│"));
+    } else {
+        cat_box.push_str(&format!("\n > ^ <{spaces} │"))
+    }
+
+    cat_box.push_str(btm_line.as_str());
+
+    if terminal_args.color {
+        let gradient_bounds = GRADIENTS[thread_rng().gen_range(0..GRADIENTS.len())];
+        print_gradient(&cat_box, gradient_bounds.0, gradient_bounds.1);
+    } else {
+        println!("{}", cat_box);
+    }
+
+    flush_styles();
+    println!();
+    println!();
 }
